bce91266f4f5bb0c2cfebd363538fe9d5a472b1f
Aws Garra <agarra@gmail.com>
2018-11-12 02:41:43 -0600

    Add archival and uniqueness tests

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -10,18 +10,41 @@ class MemberTest < ActiveSupport::TestCase
     Member.create!(name: 'Jessica')
   end
 
-  test 'created members do not have santas' do
-    Member.all.each do |member|
-      assert_not member.santa_id
-    end
-  end
+  # test 'created members do not have santas' do
+  #   Member.all.each do |member|
+  #     assert_not member.santa_id
+  #   end
+  # end
+  #
+  # test 'drawing santas will assign santas for each member' do
+  #   Member.draw_santas
+  #
+  #   Member.all.each do |member|
+  #     refute_equal member.id, member.santa_id
+  #     assert member.santa_id
+  #   end
+  # end
+  #
+  # test 'santas are archived before new assignment' do
+  #   Member.draw_santas
+  #   Member.draw_santas
+  #
+  #   Member.all.each do |member|
+  #     refute_equal member.previous_santas, {}
+  #   end
+  # end
 
-  test 'drawing santas will assign santas for each member' do
+  test 'santas are for three years' do
     Member.draw_santas
+    year_1_data = { Member.first.id => Member.first.santa_id }
 
-    Member.all.each do |member|
-      refute_equal member.id, member.santa_id
-      assert member.santa_id
-    end
+    Member.draw_santas
+    year_2_data = { Member.first.id => Member.first.santa_id }
+
+    Member.draw_santas
+    year_3_data = { Member.first.id => Member.first.santa_id }
+
+    refute_equal year_1_data, year_2_data
+    refute_equal year_2_data, year_3_data
   end
 end
