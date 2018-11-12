dd5c98ce2238b6a2541b8ab82bdddd45b14ff1d6
Aws Garra <agarra@gmail.com>
2018-11-12 01:32:02 -0600

    Test member model

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
+require 'pry'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    Member.create!(name: 'John')
+    Member.create!(name: 'Kate')
+    Member.create!(name: 'Michael')
+    Member.create!(name: 'Jessica')
+  end
+
+  test 'created members do not have santas' do
+    Member.all.each do |member|
+      assert_not member.santa_id
+    end
+  end
+
+  test 'drawing santas will assign santas for each member' do
+    Member.draw_santas
+
+    Member.all.each do |member|
+      refute_equal member.id, member.santa_id
+      assert member.santa_id
+    end
+  end
 end
